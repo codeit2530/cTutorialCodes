0c160c35788fe82b3c8affeb85492fcfd6627532
shivam kumar <srivastavshivam025@gmail.com>
2025-08-04 12:18:53 +0530

    Update shivam.c

--- shivam.c
+++ shivam.c
@@ -1,46 +1,44 @@
-// #include<stdio.h>
-// int main()
-// {
-//     char ch;
-//     printf("Enter a number\n");
-//     scanf("%c",&ch);
-//     switch (ch)
-//     {
-//     case 'A':
-//         printf("vowle\n");
-//         break;
-//     case 'E':
-//         printf("vowle\n");
-//         break;
-//     case 'I':
-//         printf("vowle\n");
-//         break;
-//      case 'O':
-//         printf("vowle\n");
-//         break;
-//     case 'U':
-//         printf("vowle\n");
-//         break;
-//         case 'a':
-//         printf("vowle\n");
-//         break;
-//         case 'e':
-//         printf("vowle\n");
-//         break;
-//         case 'i':
-//         printf("vowle\n");
-//         break;
-//         case 'o':
-//         printf("vowle\n");
-//         break;
-//         case 'u':
-//         printf("vowle\n");
-//         break;
-//     default:
-//     printf("consonate\n");
-//         break;
-//     }
-//     return 0;
-// }
-
-// Q.) 
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a number\n");
+    scanf("%c",&ch);
+    switch (ch)
+    {
+    case 'A':
+        printf("vowle\n");
+        break;
+    case 'E':
+        printf("vowle\n");
+        break;
+    case 'I':
+        printf("vowle\n");
+        break;
+     case 'O':
+        printf("vowle\n");
+        break;
+    case 'U':
+        printf("vowle\n");
+        break;
+        case 'a':
+        printf("vowle\n");
+        break;
+        case 'e':
+        printf("vowle\n");
+        break;
+        case 'i':
+        printf("vowle\n");
+        break;
+        case 'o':
+        printf("vowle\n");
+        break;
+        case 'u':
+        printf("vowle\n");
+        break;
+    default:
+    printf("consonate\n");
+        break;
+    }
+    return 0;
+}
