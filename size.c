ecdacb11f24fdc566cc15934eecdda97c06553f0
shivam kumar <srivastavshivam025@gmail.com>
2025-09-16 21:06:50 +0530

    Update size.c

--- size.c
+++ size.c
@@ -2,5 +2,11 @@
 int main()
 {
     printf("%d",sizeof(int));
+    printf("%d",sizeof(float));
+    printf("%d",sizeof(char));
+    printf("%d",sizeof(double));
+    printf("%d",sizeof(long));
+    printf("%d",sizeof(signed));
+    printf("%d",sizeof(unsigned));
     return 0;
 }
